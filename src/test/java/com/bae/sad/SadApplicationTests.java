5149f33c1b36810e8fcd0f855f39ea5e0059157f
David Bae <baeddavid@gmail.com>
2020-09-13 21:27:14 -0500

    initial commit

---
+++ src/test/java/com/bae/sad/SadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.sad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
