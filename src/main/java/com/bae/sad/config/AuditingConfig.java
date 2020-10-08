8f67f5c1ae3498471859612be865316766e8cc8a
David Bae <baeddavid@gmail.com>
2020-10-08 02:03:59 -0500

    Add basic skeleton code for JPA auditing

---
+++ src/main/java/com/bae/sad/config/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.bae.sad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+}
