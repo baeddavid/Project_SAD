93bbe728c0b5f71a6a476bbd9caad79795dafd0f
David Bae <baeddavid@gmail.com>
2020-10-08 17:02:41 -0500

    Add getters and setters for attributes

--- src/main/java/com/bae/sad/model/User.java
+++ src/main/java/com/bae/sad/model/User.java
@@ -44,6 +44,11 @@ public class User {
         @Size(max = 100)
         private String password;
 
+        @ManyToMany(fetch = FetchType.LAZY)
+        @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+        private Set<Role> roles = new HashSet<>();
         // This is where we would declare the data structure to store matches
 
        public User() { }
@@ -52,5 +57,53 @@ public class User {
                 this.name = name;
                 this.email = email;
                 this.password = password;
+        }
+
+        public Long getId() {
+               return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getUsername() {
+                return username;
+        }
+
+        public void setUsername(String username) {
+                this.username = username;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+
+        public String getPassword() {
+                return password;
+        }
+
+        public void setPassword(String password) {
+                this.password = password;
+        }
+
+        public Set<Role> getRoles() {
+                return roles;
+        }
+
+        public void setRoles(Set<Role> roles) {
+                this.roles = roles;
         }
 }
