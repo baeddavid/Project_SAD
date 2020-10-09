e418148e1d51275cf545e32583999307b633d801
David Bae <baeddavid@gmail.com>
2020-10-08 22:19:13 -0500

    add comments for JwtAuthenticatonFilter

--- src/main/java/com/bae/sad/security/JwtAuthenticationFilter.java
+++ src/main/java/com/bae/sad/security/JwtAuthenticationFilter.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    // Injecting Jwt and Userdetails dependencies
     @Autowired
     private JwtTokenProvider tokenProvider;
 
@@ -27,28 +27,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
+    // OncePerRequestFilter requires overriding doFilterInternal method
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
+            // Grab the jwt token from the request
             String jwt = getJwtFromRequest(request);
 
+            // Check if what we grabbed is a jwt token and a valid token
             if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                // Grab the user id from the jwt token
                 Long userId = tokenProvider.getUserIdFromJWT(jwt);
+                // Load user data from the user id
                 UserDetails userDetails = customUserDetailService.loadUserById(userId);
+                // Authenticate our user
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
-        } catch(Exception ex) {
+        }
+        // What we grabbed was either not a jwt token or not a valid jwt token
+        catch(Exception ex) {
             logger.error("Could not set user authentication in security context", ex);
         }
         filterChain.doFilter(request, response);
     }
 
+    // Grabs the jwt token from a HTTP Request
     private String getJwtFromRequest(HttpServletRequest request) {
+        // Grab our auth header
         String bearerToken = request.getHeader("Authorization");
+        // If it is a authentication request and we have a bearer token
         if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            // Return the jwt token
             return bearerToken.substring(7, bearerToken.length());
         }
         return null;
