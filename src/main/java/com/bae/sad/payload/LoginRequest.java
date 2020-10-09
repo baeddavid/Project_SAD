ca475fceb7b016518e7cade14417e23d2148b862
David Bae <baeddavid@gmail.com>
2020-10-08 23:19:54 -0500

    add login request class

--- src/main/java/com/bae/sad/payload/LoginRequest.java
+++ src/main/java/com/bae/sad/payload/LoginRequest.java
@@ -1,2 +1,28 @@
-package com.bae.sad.payload;public class LoginRequest {
+package com.bae.sad.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+
+    public void setUsernameOrEmail(String usernameOrEmail) {
+        this.usernameOrEmail = usernameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
