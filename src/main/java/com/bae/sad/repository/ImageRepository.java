cc2df5a74c3ea3485593482071d92a94e4a5107a
David Bae <baeddavid@gmail.com>
2020-10-14 16:45:25 -0500

    add repository for image

--- src/main/java/com/bae/sad/repository/ImageRepository.java
+++ src/main/java/com/bae/sad/repository/ImageRepository.java
@@ -1,2 +1,11 @@
-package com.bae.sad.repository;public class ImageRepository {
+package com.bae.sad.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+import com.bae.sad.model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByName(String name);
 }
