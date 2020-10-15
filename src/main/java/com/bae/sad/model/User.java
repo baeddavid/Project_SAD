1f05407bbbdd5fa21b85fa9d33b059f66949da06
David Bae <baeddavid@gmail.com>
2020-10-14 19:52:27 -0500

    Add list of images for user

--- src/main/java/com/bae/sad/model/User.java
+++ src/main/java/com/bae/sad/model/User.java
@@ -8,8 +8,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.List;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
@@ -22,134 +22,130 @@ import java.util.Set;
 })
 // TODO We need to figure out how to store user matches. Are we going to use a graph, list, or something else??? ASAP
 public class User {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        // Each user gets a random generated value for their id value
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Each user gets a random generated value for their id value
+    private Long id;
 
-        @NotBlank
-        @Size(max = 40)
-        // Name of the user
-        // Maybe let user choose to disclose name?
-        private String name;
+    @NotBlank
+    @Size(max = 40)
+    // Name of the user
+    // Maybe let user choose to disclose name?
+    private String name;
 
-        @NotBlank
-        @Size(max = 40)
-        // Username of the user. Always display this.
-        private String username;
+    @NotBlank
+    @Size(max = 40)
+    // Username of the user. Always display this.
+    private String username;
 
-        @Size(max = 40)
-        // Facebook account
-        private String facebookAccount;
+    @Size(max = 40)
+    // Facebook account
+    private String facebookAccount;
 
-        @Size(max = 40)
-        // Instagram account
-        private String instagramAccount;
+    @Size(max = 40)
+    // Instagram account
+    private String instagramAccount;
 
-        @NaturalId
-        @NotBlank
-        @Size(max = 40)
-        @Email
-        private String email;
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
 
-        @NotBlank
-        @Size(max = 100)
-        private String password;
+    @NotBlank
+    @Size(max = 100)
+    private String password;
 
-        @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-        private List<Image> pictures;
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Image> pictureList;
 
-        @ManyToMany(fetch = FetchType.LAZY)
-        @JoinTable(name = "user_roles",
-                joinColumns = @JoinColumn(name = "user_id"),
-                inverseJoinColumns = @JoinColumn(name = "role_id"))
-        private Set<Role> roles = new HashSet<>();
-        // This is where we would declare the data structure to store matches
 
-       public User() { }
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+    // This is where we would declare the data structure to store matches
 
-        public User(String name, String email, String password) {
-                this.name = name;
-                this.email = email;
-                this.password = password;
-        }
+    public User() { }
+
 
     public User(String name, String username, String email, String password) {
-           this.name = name;
-           this.username = username;
-           this.email = email;
-           this.password = password;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
 
     public Long getId() {
-               return id;
-        }
+        return id;
+    }
 
-        public void setId(Long id) {
-                this.id = id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public String getUsername() {
-                return username;
-        }
+    public String getUsername() {
+        return username;
+    }
 
-        public void setUsername(String username) {
-                this.username = username;
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-        public String getName() {
-                return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName(String name) {
-                this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public String getEmail() {
-                return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setEmail(String email) {
-                this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public void setInstagramAccount(String instagramAccount) {
-           this.instagramAccount = instagramAccount;
-        }
+    public void setInstagramAccount(String instagramAccount) {
+        this.instagramAccount = instagramAccount;
+    }
 
-        public String getInstagramAccount() {
-           return instagramAccount;
-        }
+    public String getInstagramAccount() {
+        return instagramAccount;
+    }
 
-        public void setFacebookAccount(String facebookAccount) {
-           this.facebookAccount = facebookAccount;
-        }
+    public void setFacebookAccount(String facebookAccount) {
+        this.facebookAccount = facebookAccount;
+    }
 
-        public String getFacebookAccount() {
-           return facebookAccount;
-        }
+    public String getFacebookAccount() {
+        return facebookAccount;
+    }
 
-        public String getPassword() {
-                return password;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public void setPassword(String password) {
-                this.password = password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public List<Image> getPictures() {
-           return pictures;
-        }
+    public List<Image> getPictures() {
+        return pictureList;
+    }
 
-        public void setPictures(List<Image> pictures) {
-           this.pictures = pictures;
-        }
+    public void setPictures(List<Image> pictureList) {
+        this.pictureList = pictureList;
+    }
 
-        public Set<Role> getRoles() {
-                return roles;
-        }
+    public Set<Role> getRoles() {
+        return roles;
+    }
 
-        public void setRoles(Set<Role> roles) {
-                this.roles = roles;
-        }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
